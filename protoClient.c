9d663f2efdd95f1bf1f86ca118eda288b5271c0e
antoine-bz <anboez@hotmail.fr>
2024-02-06 17:54:00 +0100

    mise en forme

--- protoClient.c
+++ protoClient.c
@@ -1,61 +1,6 @@
 #include "protoClient.h"
 socket_t sockDial;  
 
-//fct pour lancer une musique
-void lancerMusique(char *file_name, int tempsEcoule) {
-    mpg123_handle *mh;
-    unsigned char *buffer;
-    size_t buffer_size;
-    size_t done;
-    int err;
-
-    // Initialiser mpg123
-    mpg123_init();
-    mh = mpg123_new(NULL, &err);
-
-    // Ouvrir le fichier MP3
-    mpg123_open(mh, file_name);
-
-    // Configurer la sortie audio avec la bibliothèque 'ao'
-    int driver;
-    ao_device *device;
-    ao_sample_format format;
-    long rate;
-    int channels, encoding;
-    
-    ao_initialize();
-    driver = ao_default_driver_id();
-    mpg123_getformat(mh, &rate, &channels, &encoding);
-    format.bits = mpg123_encsize(encoding) * BITS;
-    format.rate = rate;
-    format.channels = channels;
-    format.byte_format = AO_FMT_NATIVE;
-    format.matrix = 0;
-    device = ao_open_live(driver, &format, NULL);
-
-    // Allouer le tampon de lecture
-    buffer_size = mpg123_outblock(mh);
-    buffer = (unsigned char*)malloc(buffer_size * sizeof(unsigned char));
-
-    // Lire et jouer le fichier MP3 à partir du temps écoulé
-    long startingFrame = (long)((tempsEcoule / 1000.0) * rate);
-    mpg123_seek_frame(mh, startingFrame, SEEK_SET);
-
-    printf("Lecture du fichier MP3\n");
-    while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK) {
-        ao_play(device, buffer, done);
-    }
-
-    // Nettoyer et fermer
-    free(buffer);
-    ao_close(device);
-    mpg123_close(mh);
-    mpg123_delete(mh);
-    mpg123_exit();
-    ao_shutdown();
-}
-
-
 void client(char *addrIPsrv, short port) {
     char reponse[MAX_BUFF];
     char musicName[MAX_BUFF];
@@ -183,4 +128,59 @@ static void signalHandler(int sig) {
             printf("Signal inconnu\n");
             break;
     }
+}
+
+
+
+void lancerMusique(char *file_name, int tempsEcoule) {
+    mpg123_handle *mh;
+    unsigned char *buffer;
+    size_t buffer_size;
+    size_t done;
+    int err;
+
+    // Initialiser mpg123
+    mpg123_init();
+    mh = mpg123_new(NULL, &err);
+
+    // Ouvrir le fichier MP3
+    mpg123_open(mh, file_name);
+
+    // Configurer la sortie audio avec la bibliothèque 'ao'
+    int driver;
+    ao_device *device;
+    ao_sample_format format;
+    long rate;
+    int channels, encoding;
+    
+    ao_initialize();
+    driver = ao_default_driver_id();
+    mpg123_getformat(mh, &rate, &channels, &encoding);
+    format.bits = mpg123_encsize(encoding) * BITS;
+    format.rate = rate;
+    format.channels = channels;
+    format.byte_format = AO_FMT_NATIVE;
+    format.matrix = 0;
+    device = ao_open_live(driver, &format, NULL);
+
+    // Allouer le tampon de lecture
+    buffer_size = mpg123_outblock(mh);
+    buffer = (unsigned char*)malloc(buffer_size * sizeof(unsigned char));
+
+    // Lire et jouer le fichier MP3 à partir du temps écoulé
+    long startingFrame = (long)((tempsEcoule / 1000.0) * rate);
+    mpg123_seek_frame(mh, startingFrame, SEEK_SET);
+
+    printf("Lecture du fichier MP3\n");
+    while (mpg123_read(mh, buffer, buffer_size, &done) == MPG123_OK) {
+        ao_play(device, buffer, done);
+    }
+
+    // Nettoyer et fermer
+    free(buffer);
+    ao_close(device);
+    mpg123_close(mh);
+    mpg123_delete(mh);
+    mpg123_exit();
+    ao_shutdown();
 }
